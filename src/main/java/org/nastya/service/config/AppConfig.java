2f335d955a95efe7821aba6aab9cd3e6e2a7549c
anassvoronn <UQzbwQCt7R3uMfz>
2025-07-09 19:35:36 +0300

    added library auth

--- src/main/java/org/nastya/service/config/AppConfig.java
+++ src/main/java/org/nastya/service/config/AppConfig.java
@@ -1,5 +1,8 @@
 package org.nastya.service.config;
 
+import org.nastya.lib.auth.AuthorizationValidator;
+import org.nastya.lib.auth.UserAuthorizationChecker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +13,15 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public UserAuthorizationChecker userAuthorizationChecker(RestTemplate restTemplate, @Value("${book-shop.authenticator}") String baseUrl){
+        return new UserAuthorizationChecker(restTemplate, baseUrl);
+    }
+
+    @Bean
+    public AuthorizationValidator authorizationValidator(UserAuthorizationChecker checker){
+        return new AuthorizationValidator(checker);
     }
 }
